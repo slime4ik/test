8b33a2d1a122fbf6769b45b874c1939f20cf0d31
Vyacheslav <vyacheslav@Vyacheslavs-MacBook-Air.local>
2026-06-24 16:43:18 +0300

    first commit

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 
 
 def health(request):
-    return JsonResponse({"status": "ОКЕЙ", "version": "228"})
+    return JsonResponse({"status": "ok", "version": "228"})
 
 
 urlpatterns = [
